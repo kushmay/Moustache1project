0aeeefac5b3a9ee78f7469cd1a083f4ff98aef5f
kushmay <81627608+kushmay@users.noreply.github.com>
2022-02-08 17:19:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+function preload(){
+    man_moustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+    }
+     function setup(){
+         canvas=createCanvas(300,300);
+         canvas.center();
+         video=createCapture(VIDEO);
+         video.size(300,300);
+         video.hide();
+         poseNet = ml5.poseNet(video, modelLoaded);
+         poseNet.on('pose' , gotPoses);
+     }
+     function modelLoaded(){
+         console.log('PoseNet Is Initialized');
+     }
+     function gotPoses(results)
+     {
+         if(results.length > 0)
+         {
+             console.log(results);
+             noseX = results[0].pose.nose.x -15;
+             noseY = results[0].pose.nose.y -15;
+             console.log("moustache x = " + noseX);
+             console.log("moustache y = " + noseY);
+             console.log("moustache x = " + results[0].pose.nose.x);
+             console.log("moustache y = " + results[0].pose.nose.y); 
+         }
+     }
+     function draw(){
+         image(video, 0, 0, 300, 300);
+      image(man_moustache, noseX, noseY, 30, 30);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
